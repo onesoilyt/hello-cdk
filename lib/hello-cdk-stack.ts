5619bf7071a1ed2d1e9c6a770e0243326bc0bb81
Yitu Xu <xuyitu@amazon.com>
2020-02-19 18:26:00 -0800

    add first bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,12 @@
-import * as cdk from '@aws-cdk/core';
+import core = require("@aws-cdk/core");
+import s3 = require("@aws-cdk/aws-s3");
 
-export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class HelloCdkStack extends core.Stack {
+  constructor(scope: core.App, id: string, props?: core.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true
+    });
   }
 }
